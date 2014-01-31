2a791ba36d4d35a0a3f2195c01ecb20f345596e6
JordanLingHua <jordan.hua@yahoo.com>
2014-01-30 22:59:39 -0800

    Up and down switched.

--- cs113Project/Assets/Player.cs
+++ cs113Project/Assets/Player.cs
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour {
 	public float rotateSpeed = 25.0f;
 	public float speed = 50.0f;
 
-
+	//private float landingSpeed = 
 	
 	// Use this for initialization
 	void Start () {
@@ -48,21 +48,36 @@ public class Player : MonoBehaviour {
 		float rotateAmount = rotateSpeed * Time.deltaTime;
 
 
+		transform.Translate(0,0,transAmount);
+
 
 		//pitch
 		if(Input.GetKey ("up"))
 		{
-			transform.Rotate (-rotateAmount, 0, 0);
+			transform.Rotate (rotateAmount, 0, 0);
 			//target.transform.localPosition = transform.position + new Vector3(0,0,-7);
+
+
 			//camera rotate down along x
-			target.transform.Rotate(transform.rotation.x,0,0);
+			//target.transform.Rotate(transform.rotation.x,0,0);//Code that caused problem.  Camera collision fixed
+			//Although, that commented out code above had the strange attribute of causing 
+			//the cube not to be centered all the time, making the fighter more "realistic".  
+			//Come back to this if have more time.  For now, it stays fixed.  
+
+
 		}
 		if(Input.GetKey ("down"))
 		{
-			transform.Rotate (rotateAmount, 0, 0);
+			transform.Rotate (-rotateAmount, 0, 0);
 			//target.transform.localPosition = transform.position + new Vector3(0,0,-7);
+
 			//camera rotate up along x
-			target.transform.Rotate (-transform.rotation.x,0,0);
+			//target.transform.Rotate (-transform.rotation.x,0,0);//Code that caused problem.  Camera collision fixed
+			//Although, that commented out code above had the strange attribute of causing 
+			//the cube not to be centered all the time, making the fighter more "realistic".  
+			//Come back to this if have more time.  For now, it stays fixed.  
+
+
 
 		}
 
@@ -80,30 +95,34 @@ public class Player : MonoBehaviour {
 		//barrel roll left
 		if(Input.GetKey ("a"))
 		{
-			transform.Rotate (0, 0, rotateAmount);
+			transform.Rotate (0, 0, (rotateAmount * 2));
 		}
 
 		//barrel roll right
 		if(Input.GetKey ("d"))
 		{
-			transform.Rotate (0, 0, -rotateAmount);
+			transform.Rotate (0, 0, (-rotateAmount * 2));
 		}
 
 		//forward
 		if(Input.GetKey ("w"))
 		{
-			transform.Translate (0, 0, transAmount);
+			transform.Translate (0, 0, transAmount * 2);
 		}
 
 		//backward
 		if(Input.GetKey ("s"))
 		{
-			transform.Translate (0, 0, -transAmount );
+			transform.Translate (0, 0, (transAmount * 0.003f) );//Does not slow down...
 		}
 		
 
-
-
+		//Finish later
+		//Landing/Takeoff
+		/*if(Input.GetKey ("t"))
+		{
+			transform.Translate(0, transform.position.y - 1,transAmount);
+		}*/
 
 
 
