c72b8ed6993d3a19cee1691fe5f622e642f51106
rafael simao <raficher@gmail.com>
2014-02-03 17:08:30 -0800

    Created the SceneMenu
    Created the scene menu with a “start” button that leads to the Scene1,
    and two other buttons (for now, useless).

---
+++ cs113Project/Assets/menu/Menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		GUI.Box(new Rect(550,80,250,300),"Metal Wars");
+
+		if(GUI.Button(new Rect(625,150,100,50),"Start")) {
+			Application.LoadLevel("Scene1");
+			Debug.Log("LoadLevel");
+		}
+
+		if(GUI.Button(new Rect(625,210,100,50),"Options")) {
+			
+		}
+
+		if(GUI.Button(new Rect(625,270,100,50),"Credits")) {
+			
+		}
+	}
+
+}
