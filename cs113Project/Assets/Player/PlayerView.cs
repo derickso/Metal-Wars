90baf61724b3c08ba95c0e29b6cfd6d38eab154a
JordanLingHua <jordan.hua@yahoo.com>
2014-02-06 21:47:02 -0800

    Third Person Camera switch to first person toggle with shift button
    Add this to design document

--- cs113Project/Assets/Player/PlayerView.cs
+++ cs113Project/Assets/Player/PlayerView.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class PlayerView : MonoBehaviour {
 
+	public Camera FirstPersonCam;
+	public Camera ThirdPersonCam;
+	private bool viewActivated = false;
+
+
 	// Use this for initialization
 	void Start () {
+
+		FirstPersonCam.enabled = false;
+		ThirdPersonCam.enabled = true;
+
 		transform.position = new Vector3(0,15.5f,-3);
 		transform.Rotate (0,0,0);
 		transform.localScale.Scale(new Vector3(1,1,1));
@@ -12,6 +21,23 @@ public class PlayerView : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.LeftShift) && viewActivated == false)
+		{
+			FirstPersonCam.enabled = true;
+			ThirdPersonCam.enabled = false;
+
+			viewActivated = true;
+		}
+		else if(Input.GetKeyDown(KeyCode.LeftShift) && viewActivated == true)
+		{
+			FirstPersonCam.enabled = false;
+			ThirdPersonCam.enabled = true; 
+
+			viewActivated = false;
+		}
+
+
 	
 	}
 }
