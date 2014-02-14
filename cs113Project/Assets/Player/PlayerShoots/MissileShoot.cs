8d778b590f90eaf76861c9694129d75a1cfdab75
derickso <derickso@uci.edu>
2014-02-14 15:27:04 -0800

    gui changes try again
    missiles, lives, start over

--- cs113Project/Assets/Player/PlayerShoots/MissileShoot.cs
+++ cs113Project/Assets/Player/PlayerShoots/MissileShoot.cs
@@ -14,24 +14,7 @@ public class MissileShoot : MonoBehaviour {
 	
 
 
-
-	//Heat-Seeking stuff
-	public GameObject target1;
-	private int totalNumOfSCOUTEnemies = 3;//Change this number when you add in more enemies
-	private GameObject[] enemies1 = new GameObject[100];
-
-
-	public GameObject target2;
-	private int totalNumOfZEROSENEnemies = 3;//Change this number when you add in more enemies
-	private GameObject[] enemies2 = new GameObject[100];
-
-	public GameObject target3;
-	private int totalNumOfCLASS3Enemies = 3;//Change this number when you add in more enemies
-	private GameObject[] enemies3 = new GameObject[100];
-
-	public GameObject target4;
-	private int totalNumOfCLASS4Enemies = 3;//Change this number when you add in more enemies
-	private GameObject[] enemies4 = new GameObject[100];
+	public GameObject target;
 
 
 	// Use this for initialization
@@ -39,28 +22,10 @@ public class MissileShoot : MonoBehaviour {
 		//goProjectilePrefab.SetActive (false);
 		for (int i = 0; i < argoProjectiles.Length; i++) 
 		{
-			argoProjectiles[i] = (GameObject)Instantiate (goProjectilePrefab,this.gameObject.transform.position,this.gameObject.transform.rotation);//
+			//argoProjectiles[i] = (GameObject)Instantiate (goProjectilePrefab,this.gameObject.transform.position,this.gameObject.transform.rotation);//
 			//argoProjectiles[i].transform.Rotate(0,0,90f);
-			argoProjectiles[i].SetActive (false);//
+			//argoProjectiles[i].SetActive (false);//
 		}
-
-
-
-
-
-		//Make all the enemy planes in the game possible for selection
-
-		for(int i = 1; i <= totalNumOfSCOUTEnemies; i++)//Don't use enemy size array list otherwise you cause null reference objects.  
-		{
-			//Use i as the prefix of the name
-			enemies1[i-1] = GameObject.Find("Scout"+i);
-
-
-		}
-
-
-
-
 
 	}
 	
@@ -68,12 +33,7 @@ public class MissileShoot : MonoBehaviour {
 	void Update () {
 	
 		//float rotateAmount = 25f * Time.deltaTime;
-<<<<<<< HEAD
 		if (Input.GetKeyDown(KeyCode.Q) && Player.numEMPs != 0) //Just hold down to shoot
-=======
-		//if (Input.GetKeyDown(KeyCode.Q) && Player.numMissiles != 0) //Just hold down to shoot
-		if (Input.GetKeyDown(KeyCode.Q))
->>>>>>> 2836010a1a205bc52c5e92be889ae96e20b852d1
 		{		
 			//GameObject go = argoProjectiles[iNext++];
 			GameObject go = (GameObject)Instantiate (goProjectilePrefab,this.gameObject.transform.position,this.gameObject.transform.rotation);
@@ -90,32 +50,21 @@ public class MissileShoot : MonoBehaviour {
 			//go.transform.rotation = transform.LookAt(transform.position + rigidbody.velocity);//
 			//go.transform.Rotate (90,0,0);//
 			go.rigidbody.AddForce (transform.forward * fMag);
-<<<<<<< HEAD
 
 			Player.healthAmount = Player.healthAmount - .125f;
 			//Player.numLives = Player.numLives - 0.5;
 			//Debug.Log (Player.numLives);
-=======
-			//Player.numMissiles = Player.numMissiles - 0.5;//Unlimited ammo for now
->>>>>>> 2836010a1a205bc52c5e92be889ae96e20b852d1
 			//temp = go;
 
 
-
-
-
-
-
 			//Heat Seeking Code:  
-
 			//Vector3 newTarget = target.transform.position - transform.position;
+			
 			//go.rigidbody.velocity =  newTarget.normalized * 800;
+			
 			//go.transform.LookAt (target.transform.position);
 
-
-
-
-
+			
 			//if()
 			//{
 			//}
@@ -124,13 +73,7 @@ public class MissileShoot : MonoBehaviour {
 
 
 
-		//Trying out the heatseeking outside here first.  
 
-
-		for(int i = 0; i < totalNumOfSCOUTEnemies; i++)
-		{
-
-		}
 
 
 
