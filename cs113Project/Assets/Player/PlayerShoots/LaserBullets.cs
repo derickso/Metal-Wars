57304bf62e9eaaf2e8f5556363c7d3f1ff2932e9
rafael simao <raficher@gmail.com>
2014-02-21 13:01:39 -0800

    Many collision changes
    Removed the force and collision effects from all the particle
    systems(again) and also commented some code lines in the bullets
    scripts. It is also with a test scout being done.

--- cs113Project/Assets/Player/PlayerShoots/LaserBullets.cs
+++ cs113Project/Assets/Player/PlayerShoots/LaserBullets.cs
@@ -23,27 +23,32 @@ public class LaserBullets : MonoBehaviour {
 
 	void OnTriggerEnter (Collider otherObject) {
 		//Debug.Log("PlayerBullets collided");
+		//if it hits an enemy:
 		if(otherObject.CompareTag("Enemy"))
 		{
+			//Make damage to the enemy
 			Enemy enemy = (Enemy)otherObject.gameObject.GetComponent("Enemy");
 			enemy.receiveDamage(damage);
 
+			//Make the hit effects
 			hit();
 		}
 	}
 
 	void OnCollisionEnter (Collision collision) {
+		//If it collides with anything else it is destroied
 		hit();
 	}
 
-
+	//On trigger collision exit
 	void OnTriggerExit (Collider otherObject) {
+		//If the bullet get out fo the map bounds it stops
 		if(otherObject.CompareTag("MapBounds"))
-		{
 			gameObject.SetActive(false);
-		}
 	}
 
+	//Function that executes the hit effect procedure,
+	//creating the hitEffect and setting the gameObject to false.
 	void hit(){
 		GameObject hit = (GameObject)Instantiate(hitEffect, transform.position, Quaternion.identity);
 		Destroy(hit,0.4f);
