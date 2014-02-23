43152d8dbd7375c25597dc636a5595ac99b2af79
JordanLingHua <jordan.hua@yahoo.com>
2014-02-23 10:54:54 -0800

    minimap created
    sry there was a typo in the last commit.

--- cs113Project/Assets/MiniMap.cs
+++ cs113Project/Assets/MiniMap.cs
@@ -4,15 +4,19 @@ using System.Collections;
 public class MiniMap : MonoBehaviour {
 
 
-	public GameObject player;
+	public GameObject playerObject;
 	public float distanceBtwnMinimapViewAndObj;
+
+	public float rotateSpeed = 25.0f;
+	public float speed = 50.0f;
+
 
 	// Use this for initialization
 	void Start () {
 	
 		//transform.position.x = player.transform.position.x;
 		//transform.position.z = player.transform.position.z;
-		transform.position = new Vector3(player.transform.position.x, distanceBtwnMinimapViewAndObj, player.transform.position.z);
+		transform.position = new Vector3(playerObject.transform.position.x, distanceBtwnMinimapViewAndObj, playerObject.transform.position.z);
 
 	}
 	
@@ -20,7 +24,34 @@ public class MiniMap : MonoBehaviour {
 	void Update () {
 	
 
-		transform.position = new Vector3(player.transform.position.x, distanceBtwnMinimapViewAndObj, player.transform.position.z);
+		float transAmount = speed * Time.deltaTime;
+		float rotateAmount = rotateSpeed * Time.deltaTime;
+
+		transform.position = new Vector3(playerObject.transform.position.x, distanceBtwnMinimapViewAndObj, playerObject.transform.position.z);
+		//transform.Rotate (0, 1, 0);
+		//transform.rotation = new Quaternion(0, playerObject.transform.rotation.y, 0, 1);
+		//transform.eulerAngles.y = playerObject.transform.eulerAngles.y;
+		//transform.rotation = new Quaternion(0, playerObject.transform.eulerAngles.y, 0, 1);
+		//transform.rotation = new Quaternion(0, playerObject.transform.rotation.z, 0, 1);
+
+		if(Input.GetKey (KeyCode.A))
+		{
+			//transform.Rotate (0, 0, rotateAmount);
+			//transform.Rotate (0, 0, playerObject.transform.rotation.y);
+			//transform.Rotate (0, 0, playerObject.transform.eulerAngles.y);
+			//transform.Rotate (0, 0, playerObject.transform.rotation.y * Time.deltaTime);
+			//transform.Rotate (0, 0, playerObject.transform.rotation.y * rotateAmount);
+
+		}
+		if(Input.GetKey (KeyCode.D))
+		{
+			//transform.Rotate (0, 0, -rotateAmount);
+			//transform.Rotate (0, 0, -(playerObject.transform.rotation.y));
+			//transform.Rotate (0, 0, -(playerObject.transform.eulerAngles.y));
+			//transform.Rotate (0, 0, -(playerObject.transform.rotation.y * Time.deltaTime));
+			//transform.Rotate (0, 0, -(playerObject.transform.rotation.y * rotateAmount));
+
+		}
 
 
 	}
