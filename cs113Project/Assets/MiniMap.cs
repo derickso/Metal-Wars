c422bc3e395c219971b0b71f88e2ac3e2afd550c
JordanLingHua <jordan.hua@yahoo.com>
2014-02-23 01:26:32 -0800

    minimal created
    now the player and the enemies need icons attached to it and translated
    up (y) so that it displays on the minimap

---
+++ cs113Project/Assets/MiniMap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMap : MonoBehaviour {
+
+
+	public GameObject player;
+	public float distanceBtwnMinimapViewAndObj;
+
+	// Use this for initialization
+	void Start () {
+	
+		//transform.position.x = player.transform.position.x;
+		//transform.position.z = player.transform.position.z;
+		transform.position = new Vector3(player.transform.position.x, distanceBtwnMinimapViewAndObj, player.transform.position.z);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		transform.position = new Vector3(player.transform.position.x, distanceBtwnMinimapViewAndObj, player.transform.position.z);
+
+
+	}
+}
