e2e6a7eac46160ee3214eb6d61a9d4c3c52ce1f4
JordanLingHua <jordan.hua@yahoo.com>
2014-02-23 19:31:11 -0800

    spawner under progress
    don't remove enemy spawner script from spawnmanager object.  Please
    take note of how enemy spawner.cs is structured so that you know how to
    use it for implementing the CUTSCENES and LEVEL Loading

--- cs113Project/Assets/EnemySpawner.cs
+++ cs113Project/Assets/EnemySpawner.cs
@@ -6,6 +6,7 @@ public class EnemySpawner : MonoBehaviour {
 
 	//This object spawns another enemy every time one is destroyed
 
+
 	//Actually,
 	//This object spawns the next premade wave of enemies everytime the previous wave
 	//is destroyed.  
@@ -13,7 +14,7 @@ public class EnemySpawner : MonoBehaviour {
 
 	//We also need to make an array of different waypoints possible.  
 	//Every time any of the enemies is spawned, it is given one of the waypoints.  
-		
+
 
 
 	//public Transform projectile2;
@@ -40,7 +41,7 @@ public class EnemySpawner : MonoBehaviour {
 	//Enemy 2
 	public GameObject goLightEnemPrefab;//LightPlane Enemy
 	private GameObject[] argoLightEnems = new GameObject[300];
-	private int iNextLightEnems = 0;
+	private int iNextLightEnem = 0;
 	private float fMagLightEnem = 600.0f;//1 Frame = 1000.  So bulletspeed = playerSpeed * 1000 to keep up
 	private float spawnNextTime2 = 0.0f;
 	private float spawnNextDelay2 = 0.08f;
@@ -48,7 +49,7 @@ public class EnemySpawner : MonoBehaviour {
 	//Enemy 3
 	public GameObject goMediumEnemPrefab;//MediumPlane Enemy
 	private GameObject[] argoMediumEnems = new GameObject[300];
-	private int iNextMediumEnems = 0;
+	private int iNextMediumEnem = 0;
 	private float fMagMediumEnem = 600.0f;//1 Frame = 1000.  So bulletspeed = playerSpeed * 1000 to keep up
 	private float spawnNextTime3 = 0.0f;
 	private float spawnNextDelay3 = 0.08f;
@@ -56,7 +57,7 @@ public class EnemySpawner : MonoBehaviour {
 	//Enemy 4
 	public GameObject goHeavyEnemPrefab;//HeavyPlane Enemy
 	private GameObject[] argoHeavyEnems = new GameObject[300];
-	private int iNextHeavyEnems = 0;
+	private int iNextHeavyEnem = 0;
 	private float fMagHeavyEnem = 600.0f;//1 Frame = 1000.  So bulletspeed = playerSpeed * 1000 to keep up
 	private float spawnNextTime4 = 0.0f;
 	private float spawnNextDelay4 = 0.08f;
@@ -72,8 +73,16 @@ public class EnemySpawner : MonoBehaviour {
 	private int lvl1wave3Mark = 3;
 
 	private int lvl1wave1Amount = 5;//5 scout planes
+	//private int numScoutsInlvl1wave1 = 5;
+	//private int numLightEnemsInlvl1wave1 = 0;
+	//private int numMediumEnemsInlvl1wave1 = 0;
+	//private int numHeavyEnemsInlvl1wave1 = 0;
 	private int lvl1wave2Amount = 2;//2 medium planes
 	private int lvl1wave3Amount = 1;//1 heavy plane
+
+	private int numEnemiesLeftInlvl1wave1Amount;//decrement when one of the planes is destroyed
+	private int numEnemiesLeftInlvl1wave2Amount;
+	private int numEnemiesLeftInlvl1wave3Amount;
 
 
 
@@ -87,20 +96,94 @@ public class EnemySpawner : MonoBehaviour {
 	private int lvl2wave2Amount = 10;// 7 scout planes, and 3 medium planes
 	private int lvl2wave3Amount = 15;// 2 heavy planes, 6 light planes, and 7 scout planes
 
+	private int numEnemiesLeftInlvl2wave1Amount;
+	private int numEnemiesLeftInlvl2wave2Amount;
+	private int numEnemiesLeftInlvl2wave3Amount;
+
 
 
 	//Level 3 has 4 waves
 
 
+	private int lvl3wave1Mark = 7;
+	private int lvl3wave2Mark = 8;
+	private int lvl3wave3Mark = 9;
+	private int lvl3wave4Mark = 10;
+	
+	
+	private int lvl3wave1Amount = 10;//10 light planes
+	private int lvl3wave2Amount = 10;// 7 scout planes, and 3 medium planes
+	private int lvl3wave3Amount = 15;// 2 heavy planes, 6 light planes, and 7 scout planes
+	private int lvl3Wave4Amount = 5;
+	
+	private int numEnemiesLeftInlvl3wave1Amount;
+	private int numEnemiesLeftInlvl3wave2Amount;
+	private int numEnemiesLeftInlvl3wave3Amount;
+	private int numEnemiesLeftInlvl3wave4Amount;
+
+
+
+
+	//Level 4 has 5 waves
+
+
+	private int lvl4wave1Mark = 11;
+	private int lvl4wave2Mark = 12;
+	private int lvl4wave3Mark = 13;
+	private int lvl4wave4Mark = 14;
+	private int lvl4wave5Mark = 15;
+	
+	
+	private int lvl4wave1Amount = 10;//10 light planes
+	private int lvl4wave2Amount = 10;// 7 scout planes, and 3 medium planes
+	private int lvl4wave3Amount = 15;// 2 heavy planes, 6 light planes, and 7 scout planes
+	private int lvl4Wave4Amount = 5;
+	private int lvl4Wave5Amount = 15;
+	
+	private int numEnemiesLeftInlvl4wave1Amount;
+	private int numEnemiesLeftInlvl4wave2Amount;
+	private int numEnemiesLeftInlvl4wave3Amount;
+	private int numEnemiesLeftInlvl4wave4Amount;
+	private int numEnemiesLeftInlvl4wave5Amount;
+
+
+
+
+	private bool enemiesOfCurrentWaveHaveBeenSpawned;
 
 
 
 
 
+	private bool waveDestroyed; //turns on and off
+	private Vector3 spawnLocation1;//changes each wave.  Use for any 1 type of enemy
+	private Vector3 spawnLocation2;//changes each wave.  Use for any 1 type of enemy
+	private Vector3 spawnLocation3;//changes each wave.  Use for any 1 type of enemy
+	private Vector3 spawnLocation4;//changes each wave.  Use for any 1 type of enemy
 
-	//Level 4 has 1 waves
+	//Spawn function:  Instantiate(GameObject object, x, y, z);  <----That's all!!!!
 
 
+
+	/*
+	private int yieldTimeMin = 2;
+	private int yieldTimeMax = 5;
+
+	static int enemyCounter = 0;
+
+	private int spawnXOffsetMin = 0;
+	private int spawnXOffsetMax = 0;
+
+	private int spawnYOffsetMin = 0;
+	private int spawnYOffsetMax = 0; 
+
+	private int spawnZOffsetMin = 0;
+	private int spawnZOffsetMax = 0;
+
+	private int defaultSpawnNumber = 5;//
+
+	private int waveNumber = 1;
+	*/
 
 
 
@@ -109,12 +192,16 @@ public class EnemySpawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
+		//Don't know if we need Code from 'Outline' to 'Enemy 4'.  Actually, we need this because
+		//We pre-spawn all of the objects without the x,y,z position argument in the game.  
+		//The x,y, and z addition for each instatiate call shown below will be called when the
+		//object has been accessed.  
 
 		//Outline  
 		for (int i = 0; i < argoProjectiles.Length; i++) 
 		{
 			argoProjectiles[i] = (GameObject)Instantiate (goProjectilePrefab);
-			argoProjectiles[i].SetActive (false);
+			argoProjectiles[i].SetActive (false);//Don't let them appear on the map yet
 		}
 
 
@@ -146,16 +233,38 @@ public class EnemySpawner : MonoBehaviour {
 
 
 
-		//Randomize _WaypointStartScripti Somewhere in here.  Put anywhere!  
+		//Randomize _WaypointStartScripti Somewhere in here.  Put anywhere!//Ignore this for now  
+
+
+
+		numEnemiesLeftInlvl1wave1Amount = 5;//decrement when one of the planes is destroyed in this wave
+		numEnemiesLeftInlvl1wave2Amount = 2;
+		numEnemiesLeftInlvl1wave3Amount = 1;
+
+		numEnemiesLeftInlvl2wave1Amount = 10;
+		numEnemiesLeftInlvl2wave2Amount = 10;
+		numEnemiesLeftInlvl2wave3Amount = 15;
+
+		numEnemiesLeftInlvl3wave1Amount = 0;
+		numEnemiesLeftInlvl3wave2Amount = 0;
+		numEnemiesLeftInlvl3wave3Amount = 0;
+		numEnemiesLeftInlvl3wave4Amount = 0;
+
+		numEnemiesLeftInlvl4wave1Amount = 0;
+		numEnemiesLeftInlvl4wave2Amount = 0;
+		numEnemiesLeftInlvl4wave3Amount = 0;
+		numEnemiesLeftInlvl4wave4Amount = 0;
+		numEnemiesLeftInlvl4wave5Amount = 0;
 
 
 
 
 
+		enemiesOfCurrentWaveHaveBeenSpawned = false;
 
+		waveDestroyed = false;
 
-
-
+		levelAndWaveMarker = 1;
 
 
 
@@ -166,21 +275,187 @@ public class EnemySpawner : MonoBehaviour {
 	void Update () {
 		
 		
-		//Randomize _WaypointUpdateScripti Somewhere in here.  Put anywhere!  
+		//Randomize _WaypointUpdateScripti Somewhere in here.  Put anywhere!//Ignore this for now  
 		
 
-		if (Input.GetKey(KeyCode.Space)) //Just hold down to shoot
-		{
+		//if (Input.GetKey(KeyCode.Space)) //Just hold down to shoot
+		//{
 
 			
 
-		}
+		//}
 		
 		//Destroy(temp);
 
 
+		//_____Spawn Code here______________
 
 
+
+		//for(int i = 0; i < maxWaveCount; i++
+
+		//While the wave is not destoryed
+		//if(waveDestroyed == false)
+		//{
+
+
+
+
+
+		//	if(true)
+		//	{
+		//		waveDestroyed = true;
+		//	}
+		//}
+
+
+
+
+
+
+
+		if(waveDestroyed == true)
+		{
+			levelAndWaveMarker++;
+			waveDestroyed = false;
+		}
+
+
+		if(levelAndWaveMarker == 1)//Level 1 wave 1
+		{
+			/*
+			private int lvl1wave1Mark = 1;
+			private int lvl1wave2Mark = 2;
+			private int lvl1wave3Mark = 3;
+			
+			private int lvl1wave1Amount = 5;//5 scout planes
+			//private int numScoutsInlvl1wave1 = 5;
+			//private int numLightEnemsInlvl1wave1 = 0;
+			//private int numMediumEnemsInlvl1wave1 = 0;
+			//private int numHeavyEnemsInlvl1wave1 = 0;
+			private int lvl1wave2Amount = 2;//2 medium planes
+			private int lvl1wave3Amount = 1;//1 heavy plane
+			
+			private int numEnemiesLeftInlvl1wave1Amount;//decrement when one of the planes is destroyed
+			private int numEnemiesLeftInlvl1wave2Amount;
+			private int numEnemiesLeftInlvl1wave3Amount;
+			*/
+
+
+			//for(int i = 0; i < lvl1wave1Amount; i++)
+			//{
+			//}
+
+			if(enemiesOfCurrentWaveHaveBeenSpawned == false)//Spawn wave only if wave wasn't spawned yet
+				//Necessary since we are in the update function that's called in a loop.  
+			{
+				//Spawn enemies of this specific wave here
+				SpawnScout ();
+				SpawnScout ();
+				SpawnScout ();
+				SpawnScout ();
+				SpawnScout ();
+
+
+				enemiesOfCurrentWaveHaveBeenSpawned = true;
+			}
+
+
+			if(true)//enemy 1 is destroyed
+			{
+
+				numEnemiesLeftInlvl1wave1Amount--;
+
+				//if(numEnemiesLeftInLvl1wave1Amount == 0)
+				//{
+				//	levelAndWaveMarker++;
+				//}
+
+			}
+
+			if(true)//enemy 2 is destroyed
+			{
+				numEnemiesLeftInlvl1wave1Amount--;
+				
+
+			}
+
+			if(numEnemiesLeftInlvl1wave1Amount == 0)
+			{
+				levelAndWaveMarker++;
+			}
+
+
+
+
+		}
+		else if(levelAndWaveMarker == 2)//Level 1 wave 2
+		{
+
+
+
+
+
+
+		}
+		else if(levelAndWaveMarker == 3)//Level 1 wave 3
+		{
+
+
+
+
+
+		}
+		else if(levelAndWaveMarker == 4)//Level 2 wave 1
+		{
+
+
+
+
+		}
+		else if(levelAndWaveMarker == 5)//Level 2 wave 2
+		{
+
+
+
+
+
+		}
+		else if(levelAndWaveMarker == 6)//Level 2 wave 3
+		{
+
+
+
+
+
+		}
+		else if(levelAndWaveMarker == 7)//Level 3 wave 1
+		{
+		}
+		else if(levelAndWaveMarker == 8)//Level 3 wave 2
+		{
+		}
+		else if(levelAndWaveMarker == 9)//Level 3 wave 3
+		{
+		}
+		else if(levelAndWaveMarker == 10)//Level 3 wave 4
+		{
+		}
+		else if(levelAndWaveMarker == 11)//Level 4 wave 1
+		{
+		}
+		else if(levelAndWaveMarker == 12)//Level 1 wave 2
+		{
+		}
+		else if(levelAndWaveMarker == 13)//Level 1 wave 3
+		{
+		}
+		else if(levelAndWaveMarker == 14)//Level 1 wave 4
+		{
+		}
+		else if(levelAndWaveMarker == 15)//Level 1 wave 5
+		{
+		}
 
 
 
@@ -194,6 +469,11 @@ public class EnemySpawner : MonoBehaviour {
 
 
 
+
+	//Don't know if we will need these: //Actually we do.  This is the code that accesses from 
+	//the list of already instantiated enemies.  We don't want to say Instantiate(...) every time.  
+	//We want to just 
+
 	void SpawnObject () 
 	{
 		
@@ -205,6 +485,10 @@ public class EnemySpawner : MonoBehaviour {
 		//Fire the projectile
 		go.SetActive (true);
 		//go.rigidbody.velocity = Vector3.zero;
+
+		//Object's waypoint goes here.  Replace this with something else, or not use this at all
+		//if the waypoints are already inside each object.  
+		//Chase AI code will replace waypoint code at same area:  
 		go.transform.position = transform.position + transform.forward*10;
 		go.transform.rotation = transform.rotation;
 		go.rigidbody.velocity = transform.forward * fMag;
@@ -217,6 +501,15 @@ public class EnemySpawner : MonoBehaviour {
 	{
 		//Use SpawnObject outline
 
+		GameObject go = argoScouts[iNextScout++];
+		
+		if (iNextScout >= argoScouts.Length) iNextScout = 0;
+		
+		//Fire the projectile
+		go.SetActive (true);
+
+		//Offset the scout by a little every time the one before spawned.  
+
 
 
 
@@ -228,7 +521,12 @@ public class EnemySpawner : MonoBehaviour {
 	{
 		//Use SpawnObject outline
 
-
+		GameObject go = argoLightEnems[iNextLightEnem++];
+		
+		if (iNextLightEnem >= argoLightEnems.Length) iNextLightEnem = 0;
+		
+		//Fire the projectile
+		go.SetActive (true);
 
 
 
@@ -240,7 +538,12 @@ public class EnemySpawner : MonoBehaviour {
 	{
 		//Use SpawnObject outline
 
-
+		GameObject go = argoMediumEnems[iNextMediumEnem++];
+		
+		if (iNextMediumEnem >= argoMediumEnems.Length) iNextMediumEnem = 0;
+		
+		//Fire the projectile
+		go.SetActive (true);
 
 
 
@@ -254,7 +557,12 @@ public class EnemySpawner : MonoBehaviour {
 	{
 		//Use SpawnObject outline
 
-
+		GameObject go = argoHeavyEnems[iNextHeavyEnem++];
+		
+		if (iNextHeavyEnem >= argoHeavyEnems.Length) iNextHeavyEnem = 0;
+		
+		//Fire the projectile
+		go.SetActive (true);
 
 
 
